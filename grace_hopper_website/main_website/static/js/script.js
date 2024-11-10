24d7d47d101719e1e9f4e68c70cf3f24e9741b08
lemonadeskyblock <zbm.michelle@gmail.com>
2024-11-09 22:57:28 -0500

    m

--- grace_hopper_website/main_website/static/js/script.js
+++ grace_hopper_website/main_website/static/js/script.js
@@ -1,57 +1,316 @@
  
-function openCloseNavBar(openCloseNavBarText) {
-    
+async function openCloseNavBar(openCloseNavBarText) {
     const navMenu = document.getElementById("navMenu");
+    let navPos = navMenu.getBoundingClientRect();
+    let navPosleft = - navPos.width;
 
+    if (navPos.left === 0 || navPos.left === navPosleft) {    
+        if (navMenu.style.display === "none") {
+            navMenu.style.display = "block"
+            navMenu.classList.add('in-progress');
+            let slideIn = KUTE.fromTo(navMenu,
+                {left:'-250vw'},
+                {left:'0vw'},
+                {
+                duration: 500
+                }
+            );
+            openCloseNavBarText.style.color= "black";
+            awaitSlideIn = await slideIn.start();
+            navMenu.classList.add('in-view');
+            
+        } else {
+            
+            
+            let slideOut = KUTE.fromTo(navMenu,
+                {left:'0vw'},
+                {left: navPosleft},
+                {
+                    duration: 500
+                    }
+            );
+            
+            slideOutAwait = await slideOut.start();
+            
+            const twoS = {
+                waitTwoSeconds: function() {
+                setTimeout(() => {
+                    navMenu.style.display="none"; 
+                }, 500);
+                }
+            };
+            twoS.waitTwoSeconds();
+            navMenu.classList.remove('in-view');
+            openCloseNavBarText.style.color = "white"; 
+            
+            
+        } 
+    }
+}
+/*https://wearetechwomen.com/inspirational-quotes-grace-hopper-computer-scientist-u-s-navy-rear-admiral/#:~:text=“Leadership%20is%20a%20two%2Dway,half%20empty%20nor%20half%20full.*/
+var index = 0
+var quotesArray = [];
+async function loadQuotes() {
+    const requestjson = new Request("quotes.json");
+    fetch(requestjson)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(JSON.stringify(data));
+             
+            for (let i in data) {
+                quotesArray.push(data[i]);
+            }
+            var quotesSpace  = document.getElementById("quotes");
+            function fill ()    
+            {
+                if (index == 8) 
+                    {
+                    index = 0;
+                    }
+                console.log(`index: ${index}`);
+                writeText(quotesSpace, quotesArray[index]);
+                index++;
+            }
+            console.log(`time: ${quotesArray[index].length * 100}`)
+            let lengthtxt = quotesArray[index].length 
+            let calcwaitTime = (lengthtxt) => (lengthtxt < 100 ? lengthtxt * 120 / 1.5 : lengthtxt * 250);
+            let waitingTime = calcwaitTime(lengthtxt);
+            const fillQuoteSpace = setInterval(fill, waitingTime);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+function loadGlobal() {
+    
+     
+        
+     
+}
 
+function inView(element) {
+    let rect = element.getBoundingClientRect();
+    let viewHeight = Math.max(document.documentElement.clientHeight,window.innerHeight);
+    
+    if (!(rect.bottom < 0 || rect.top - viewHeight >= 0)) {
+        
+        return true
+    } else {
+        return false
+    }
+}
 
-    if (navMenu.style.display === "none") {
-        navMenu.style.display = "block"
-
-        let slideIn = KUTE.fromTo(navMenu,
-            {left:'-250vw'},
-            {left:'0vw'}
+function blurContentAnimation(element) {
+    if (!element.classList.contains('in-view')) {
+        let BlurtoNormal = KUTE.fromTo(element,
+            {opacity:'0'},
+            {opacity:'1.0'},
+            {
+                
+                duration: 5000
+            }
         );
-        slideIn.start();
+        BlurtoNormal.start();
+         
+        element.classList.add('in-view');
+        console.log('blur content');
+    }
+}
+ 
+let screenWidth = window.screen.width;
 
-        openCloseNavBarText.style.color= "black";
+function growthAn(element) {
+    
+    if (!element.classList.contains('in-view')) {
+        let growth = KUTE.fromTo(element,
+            
+            {width:'0'},
+            {width: screenWidth},
+            {
+                delay: 1200,
+                duration: 5000
+            }
+            
+
+        );
+        growth.start();
+        element.classList.add('in-view');
+    }  
+
+}
+function growthAnReversed(element) {
+    if (element.classList.contains('in-view')) {
+        let growthr = KUTE.fromTo(element,
+            
+            {width: screenWidth},
+            {width: '0'},
+            {
+                delay: 0,
+                duration: 3000
+            }
+            
+
+        );
+        growthr.start();
+        element.classList.remove('in-view');
+
+    }
+}
+function slideInAnimationRight(element) {
+    if (!element.classList.contains('in-view')) {
+        let slideInAn = KUTE.fromTo(element,
+            {right:'480vw'},
+            {right:'0vw'},
+            {
+                delay: 500,
+                duration: 2000
+            }
+            
+
+        );
+        slideInAn.start();
+        element.classList.add('in-view');
+    }
+
+}
+async function loadsection2(imagebackground, textTitle, textContainer, toggleT) {
+    if (!imagebackground.classList.contains('in-load')) {
+            try {
+                imagebackground.classList.add('in-load');
+                blurContentAnimation(imagebackground);
+                const a = await new Promise(resolve => setTimeout(resolve, 55));
+                slideInAnimation(textTitle);
+                textTitle.addEventListener("click", () => {
+                    console.log("cliskc")
+                    dropDown(toggleT, textContainer);
+                })
+                
+            /*slideInAnimationRight(textContainer); */
+            
+            
+        } catch (error) {
+            reject(error);
+        }
+    }
+    
+
+}
+function slideInAnimation(element) {
+    if (!element.classList.contains('in-view')) {
+        let slideInL = KUTE.fromTo(element,
+            {left:'480vw'},
+            {left:'0vw'},
+            {
+                delay: 500,
+                duration: 2000
+            }
+            
+
+        );
+        slideInL.start();
+        element.classList.add('in-view');
+    }
+}
+async function writeText(TextElement, txt) {
+    
+    let stext = txt;
+    let calcwaitTime = (stext) => (stext.length < 90 ? stext.length / 1.5 : stext.length);
+    let waitTime = calcwaitTime(stext);
+    TextElement.textContent = "";
+    console.log(txt);
+    
+    console.log(typeof txt);
+    console.log(stext.length);
+    console.log(`waittime: ${calcwaitTime(stext)}`);
+    console.log(`waittime: ${stext.length/1.5}`);
+    for (let i = 0; i < txt.length; i++) {
+        TextElement.textContent+= txt.charAt(i);
+        
+        
+        await new Promise(resolve => setTimeout(resolve, 55));
+         
+    }
+     
+}
+
+function dropDown(toggleElement, ElementToggled) {
+    toggleElement.style.transform = 'rotate(0deg)';
+    distanceUpDown = -toggleElement.getBoundingClientRect().height
+    if (ElementToggled.style.display === 'block') {
+        /* close dropdown*/
+        let dropDownHideDropdown = KUTE.fromTo(ElementToggled,
+            {top:'0px'},
+            {top: distanceUpDown},
+            {
+                delay: 100,
+                duration: 1000
+            }
+        );
+        dropDownHideDropdown.start();  
+         
+        const twoS = {
+            waitTwoSeconds: function() {
+            setTimeout(() => {
+                ElementToggled.style.display = 'none';
+            }, 800);
+            }
+        };
+        twoS.waitTwoSeconds();
          
          
     } else {
-        let slideOut = KUTE.fromTo(navMenu,
-            {left:'0vw'},
-            {left:'-350vw'}
-        );
-        slideOut.start();
-        const twoS = {
-            waitTwoSeconds: function() {
-               
-              setTimeout(() => {
-                navMenu.style.display="none";
-                
-              }, 1000);
-            }
-          };
-          
-          
-        twoS.waitTwoSeconds();
-        openCloseNavBarText.style.color= "white";
+        /* Ideally get height of toggle elemet to make substraction */
+        /* open dropdown*/
+        toggleElement.style.transform = 'rotate(180deg)';
         
+        let dropDownRevealDropdown = KUTE.fromTo(ElementToggled,
+            {top: distanceUpDown },
+            {top:'0px'},
+            {
+                duration: 1000
+            }
+        );
+        dropDownRevealDropdown.start(); 
+        ElementToggled.style.display = 'block';
+         
+        
+         
         
     }
-     
+
 }
 /** can be changed to arrow function remember arrow an this are immediatly invoked */
 document.addEventListener("DOMContentLoaded", function() {
     
     
-    /**
-     * 
-     * openCloseNavBarText.addEventListener("click", function() {
+
+
+    /*openCloseNavBarText.addEventListener("click", function() {
         openCloseNavBar(openCloseNavBarText);
-    })
-     * 
-     */
+    */
     
 
 })
+function loadGlobal() {
+    
+    fetch('skeleton.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById("globalnav").innerHTML = html;
+        console.log("result");
+        return true
+    })
+    .then(success => {
+        if (success) {
+            openCloseNavBarText.addEventListener( "click", function() {
+                openCloseNavBar(openCloseNavBarText);
+            })
+            loadQuotes();
+        
+        }
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+}
+
--- grace_hopper_website/main_website/views.py
+++ grace_hopper_website/main_website/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def landing_page(request):
-    return render(request, 'main.html')
+    return render(request, 'contributor.html')
